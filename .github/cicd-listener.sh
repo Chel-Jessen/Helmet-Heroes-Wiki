1a342d42a2054e7b05ae2b836135641554780a37
Chel-Jessen <chel.jessen@gmail.com>
2023-04-27 20:29:28 +0200

    Changed port and removed Custom-Header

--- .github/cicd-listener.sh
+++ .github/cicd-listener.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 while true; do
-  # Listen for incoming connections on port 8080
-  request=$(echo -ne "HTTP/1.1 200 OK\r\n\r\n" | nc -l -p 8080)
+  # Listen for incoming connections on port 6402
+  request=$(echo -ne "HTTP/1.1 200 OK\r\n\r\n" | nc -l -p 6402)
 
   # Check for requests on the "/github" route
   if [[ "$request" =~ /github ]]; then
-    # Check for custom header
-    if [[ "$request" =~ "X-Cicd-Header: true" ]]; then
-      # Perform git pull in git directory
-      cd /Helmet-Heroes-Wiki && git pull
-      # Run docker-compose up --build
-      docker-compose up --build
-    fi
+    # Perform git pull in git directory
+    cd /Helmet-Heroes-Wiki && git pull
+    # Run docker-compose up --build
+    docker-compose up --build
   fi
 done
